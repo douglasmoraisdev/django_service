b8093744ce22dccdc5da1a20a5f5fe9465be6457
Douglas Morais <douglas.morais@agristamp.com.br>
2021-03-31 17:55:56 -0300

    try fix urls

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
+from django.conf.urls import url
 from django.urls import include, path
 
 urlpatterns = [
-    path('homolog/django_service/polls/', include('polls.urls')),
-    path('homolog/django_service/admin/', admin.site.urls),
+    url(r'^homolog/'), include([
+        path('django_service/polls/', include('polls.urls')),
+        path('django_service/admin/', admin.site.urls),
+    ])
 ]
