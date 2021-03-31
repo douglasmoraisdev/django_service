8c42c7e7f19a34ddfa3917942a77d4d1dfd4647c
Douglas Morais <douglas.morais@agristamp.com.br>
2021-03-31 18:30:50 -0300

    Fix urls

--- app/app/urls.py
+++ app/app/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('/django_service/polls/', include('polls.urls')),
-    path('/django_service/admin/', admin.site.urls),
+    path('django_service/polls/', include('polls.urls')),
+    path('django_service/admin/', admin.site.urls),
 ]
